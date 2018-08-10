d08c4bb8372abff2bbb4761978603a92bf9e1903
Austin Talbot <austintalbot@gmail.com>
2018-08-09 22:41:15 -0600

    objective 1-display all, and 2-edit, delete links, complete

--- docroot/CRUD/webserver.py
+++ docroot/CRUD/webserver.py
@@ -1,37 +1,40 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
+# import CRUD Operations from Lesson 1
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+# Create session and connect to DB
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 class webServerHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            if self.path.endswith("/hello"):
+            if self.path.endswith("/restaurants"):
+                restaurants = session.query(Restaurant).all()
+                output = ""
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
-                output = ""
                 output += "<html><body>"
-                output += "<h1>Hello!</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+                for restaurant in restaurants:
+                    output += restaurant.name
+                    output += "</br>"
+                    output += "<a href=http://localhost:8080/edit>Edit</a>"
+                    output += "</br>"
+                    output += "<a href=http://localhost:8080/delete>Delete</a>"
+                    output += "</br>"
+
                 output += "</body></html>"
                 self.wfile.write(output)
-                print output
                 return
-
-            if self.path.endswith("/hola"):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                output = ""
-                output += "<html><body>"
-                output += "<h1>&#161 Hola !</h1>"
-                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-                output += "</body></html>"
-                self.wfile.write(output)
-                print output
-                return
-
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
@@ -61,7 +64,7 @@ def main():
     try:
         port = 8080
         server = HTTPServer(('', port), webServerHandler)
-        print "Web Server running on port %s" % port
+        print "Web Server running... open localhost:8080/restaurants in your browser"
         server.serve_forever()
     except KeyboardInterrupt:
         print " ^C entered, stopping web server...."
